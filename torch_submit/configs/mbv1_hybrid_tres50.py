abf9029201bf37cc4abe3b3400f827242f006715
LcDog <12553253+LcDog@users.noreply.github.com>
2019-09-16 13:53:45 -0400

    update

--- torch_submit/configs/mbv1_hybrid_tres50.py
+++ torch_submit/configs/mbv1_hybrid_tres50.py
@@ -74,9 +74,9 @@ data = dict(
         num_workers=8,
         dataset_cfg=dict(root="./data/val")))
 # optimizer
-optimizer = dict(type='SGD', lr=0.3, momentum=0.9, weight_decay=4e-5)
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=4e-5)
 # learning policy
-lr_config = dict(policy='cosine', warmup='linear', warmup_iters=2400, target_lr=1e-4, by_epoch=False)
+lr_config = dict(policy='cosine', warmup='linear', warmup_iters=5000, target_lr=1e-4, by_epoch=False)
 # misc settings
 log_config = dict(
     interval=200,
